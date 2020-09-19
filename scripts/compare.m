a68646bf2dee1442f3f7f77a99af979cc8edea0c
Allen <allenye66@gmail.com>
2020-09-19 01:45:43 -0700

    new compare method
    k = 1: 65%
    k = 2: 65%
    k = 10: 65.6%
    k = 20: 64.88%
    k = 50: 64.84%
    k = 100: 64.8%

--- scripts/compare.m
+++ scripts/compare.m
@@ -1,11 +1,18 @@
-function [] = similarity(ans1, ans2)
-[s1, s2] = size(ans1)
-count = 0;
+function [] = compare(offline_output, online_output, c_vector)
+[s1, s2] = size(offline_output)
+offline = 0
+online = 0
 for k = 1:s1;
-    if round(ans1(k)) == round(ans2(k))
-        count = count + 1;
-    end
-    
+    offline = offline + offline_output(k)*c_vector(k)
+    online = online + online_output(k)*c_vector(k)
 end
-disp(count)
-disp(count/s1)
+disp("---------------------------")
+disp(offline)
+disp(online)
+if online>offline;
+    disp(offline/online)
+end
+
+if offline>online;
+    disp(online/offline)
+end
