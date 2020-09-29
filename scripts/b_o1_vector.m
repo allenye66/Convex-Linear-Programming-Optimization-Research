da671c4448f498c0f6c1c958ff192e2deae91a10
Allen <allenye66@gmail.com>
2020-09-28 23:52:48 -0700

    comparing the b O(1) vector to previous results

---
+++ scripts/b_o1_vector.m
@@ -0,0 +1,45 @@
+m = 1e2;
+n = 1e6;
+
+
+%n is 10^6
+%m is 10 or 100
+
+
+
+
+%A is constrained between -1 and 1
+%a is all 1's and 0's or -1 and 0
+%first half pos, second negative
+%half positive and half negative values
+%in future, try if random number to indicate how many positive parts and
+%negative
+
+
+lower = -1;
+upper = 1;
+
+c = (upper-lower).*rand(n,1) + lower;
+
+A = (upper-lower).*rand(m,n) + lower;
+%need to separate it into half positive half negative
+%make two seperate vecotrs and combine them
+
+
+
+b = rand(1,m);
+% around 1000's 
+% b = [1000, 3000, 1500, 2000, 2200...]
+%change b to o(1) VECTOR
+%permutation negative part
+%b has to be positive
+%not close to zero
+
+
+
+
+
+
+%run the online solution with new input
+%run the offline solution with new input
+%compare similarity
