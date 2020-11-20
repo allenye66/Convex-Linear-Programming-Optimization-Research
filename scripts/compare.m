93839b1fc04f7bd8593c1810cd5cd18744fda8b5
Allen <allenye66@gmail.com>
2020-11-20 08:50:02 -0800

    added code to run algorithm mulitple times

--- scripts/compare.m
+++ scripts/compare.m
@@ -15,3 +15,16 @@ end
 if offline>online;
     disp(online/offline)
 end
+
+
+%% to run the algorithm 100 times:
+%x1_avg = 0
+%load input data
+%for i = 1:100
+%   [x1, y1] = fastLP(A, b, c, 1)    
+%   x1_avg = x1_avg + c'*x1
+%end
+%load o(n) or o(1) offline - make sure to get the correct one
+%offline = c'*x*100
+%offline/x1_avg
+%%
